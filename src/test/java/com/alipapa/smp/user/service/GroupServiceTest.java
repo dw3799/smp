66708a51c613f9066c83a39f12a68eb4f5257b66
maibahe <maibahe@wacai.com>
2018-03-12 19:19:07 +0800

    测试用例

---
+++ src/test/java/com/alipapa/smp/user/service/GroupServiceTest.java
@@ -0,0 +1,36 @@
+package com.alipapa.smp.user.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GroupServiceTest {
+
+    @Test
+    public void getGroupById() {
+    }
+
+    @Test
+    public void getGroupByGroupNo() {
+    }
+
+    @Test
+    public void updateGroup() {
+    }
+
+    @Test
+    public void addGroup() {
+    }
+
+    @Test
+    public void listAllGroupSelect() {
+    }
+
+    @Test
+    public void listGroupByParams() {
+    }
+
+    @Test
+    public void getLatestGroupId() {
+    }
+}
