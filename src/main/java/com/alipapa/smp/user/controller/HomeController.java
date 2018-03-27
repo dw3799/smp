dce29b5116b9721018ca3ec5025c1b87c25f8534
maibahe <maibahe@wacai.com>
2018-03-27 19:30:47 +0800

    bug修改

---
+++ src/main/java/com/alipapa/smp/user/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.alipapa.smp.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Home Controller
+ *
+ * @author maibahe
+ * @version 1.0
+ * @date 2018-03-19
+ */
+
+@RestController
+public class HomeController {
+    private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/check_active.html")
+    public String check_backend_active() {
+        return "I'm ok !";
+    }
+
+}
